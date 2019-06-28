857f532d80438a9de2ae45c363a76119c0e4d928
kthor93 <teahorsen@gmail.com>
2019-06-27 22:00:42 -0400

    .

--- server.js
+++ server.js
@@ -3,27 +3,22 @@ const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 8080;
 
-// Initialize Express
 const app = express();
 
-// Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Make public a static folder
 app.use(express.static("public"));
 
-// Connect to the Mongo DB
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/webscrape-app";
 
 mongoose.connect(MONGODB_URI);
-// mongoose.connect("mongodb://localhost/webscrape-app", { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost/webscrape-app", { useNewUrlParser: true });
 
 // Routes
-// require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
 
-// Start the server
+// Server Start
 app.listen(PORT, function () {
     console.log("App running on port " + PORT);
 });
